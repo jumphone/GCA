51de11ccd5e398b121891bd30d8884cd6f172c56
Zhang, Feng <jumphone@163.com>
2018-06-12 11:45:37 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+NORM_EXP=$1
+NET_FILE=$2
+OUTPUT=$3 
+CPU=$4
+
+SEED=321
+PERCENT=0.1
+PYTHON="python"
+RSCRIPT="Rscript"
+
+$PYTHON step2_BuildIndex.py $NET_FILE $NORM_EXP $OUTPUT\.data $PERCENT
+$PYTHON step3_CalZmat.py $OUTPUT\.data $OUTPUT\.data.zmat $CPU
+$PYTHON step4_rmSymDist.py $OUTPUT\.data.zmat $OUTPUT\.data.zmat.nosym
+$RSCRIPT step5_deMix.R $NORM_EXP $OUTPUT\.data.zmat.nosym $OUTPUT\.data.zmat.nosym $OUTPUT\.data.zmat.nosym.gca $CPU $SEED
+
+
