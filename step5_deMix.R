df46b5ed9ea0cd47f099ff249498e1c901805f7b
Zhang, Feng <jumphone@163.com>
2018-06-12 20:49:55 -0400

    Update step5_deMix.R

--- step5_deMix.R
+++ step5_deMix.R
@@ -281,14 +281,16 @@ pdf(paste0(TMP_DIR,'/G.pdf'),width=20,height=20)
 plot( c(1:51)/100,c(1:51)/100, col=colors, ylab='Score', xlab='Score', pch=16,cex=5,lwd=5,type='p',main='Edge Color Key')
 
 #l <- layout_with_fr(g)
-plot(main='All', g, layout=layout_with_fr, vertex.label.cex=0.5, vertex.size=as.matrix(node.size), vertex.label.dist=0, vertex.label.color = "black",vertex.frame.color = "white",vertex.color = "gold2")
+#plot(main='All', g, layout=layout_with_fr, vertex.label.cex=0.5, vertex.size=as.matrix(node.size), vertex.label.dist=0, vertex.label.color = "black",vertex.frame.color = "white",vertex.color = "gold2")
+plot(main='All', g, vertex.label.cex=0.5, vertex.size=as.matrix(node.size), vertex.label.dist=0, vertex.label.color = "black",vertex.frame.color = "white",vertex.color = "gold2")
 
 V(g)$comp <- components(g)$membership
 i=1
 while(i<=max(V(g)$comp)){
     this_subg = induced_subgraph(g,V(g)$comp==i)
     #l <- layout_with_fr(this_subg)
-    plot(main=paste0('SubGraph',as.character(i)),this_subg, layout=layout_with_fr, vertex.label.cex=0.5, vertex.size=as.matrix(node.size), vertex.label.dist=0, vertex.label.color = "black",vertex.frame.color = "white",vertex.color = "gold2")
+    #plot(main=paste0('SubGraph',as.character(i)),this_subg, layout=layout_with_fr, vertex.label.cex=0.5, vertex.size=as.matrix(node.size), vertex.label.dist=0, vertex.label.color = "black",vertex.frame.color = "white",vertex.color = "gold2")
+    plot(main=paste0('SubGraph',as.character(i)),this_subg, vertex.label.cex=0.5, vertex.size=as.matrix(node.size), vertex.label.dist=0, vertex.label.color = "black",vertex.frame.color = "white",vertex.color = "gold2")
     i=i+1}
 dev.off()
 #######################
