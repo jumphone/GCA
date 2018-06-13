e4de8a15e01596323f0a53f3e2d21103b3fee528
Zhang, Feng <jumphone@163.com>
2018-06-13 13:24:53 -0400

    Update and rename step1_normData.R to step0_normData.R

--- step0_normData.R
+++ step0_normData.R
@@ -1,12 +1,10 @@
 library(Seurat)
 library(dplyr)
 library(Matrix)
+suppressPackageStartupMessages(library(edgeR))
 
-#args = commandArgs(trailingOnly=TRUE)
 
 exp_data=read.table('10X_Tumor72017_matrix.txt.uniq.txt',header=T,row.names=1,sep='\t')
-
-
 EXP = CreateSeuratObject(raw.data = exp_data, min.cells = 3, min.genes=200)
 mito.genes <- grep(pattern = "^mt-", x = rownames(x = EXP@data), value = TRUE)
 percent.mito <- colSums(EXP@data[mito.genes, ]) / colSums(EXP@data)
@@ -20,8 +18,13 @@ GenePlot(object = EXP, gene1 = "nUMI", gene2 = "nGene")
 dev.off()
 
 EXP=FilterCells(object = EXP, subset.names = c("nGene", "percent.mito"), low.thresholds = c(500, -Inf), high.thresholds = c(6000, 0.05))
-EXP=NormalizeData(object = EXP, normalization.method = "LogNormalize", scale.factor = 10000)
+#EXP=NormalizeData(object = EXP, normalization.method = "LogNormalize", scale.factor = 10000)
 
-save(EXP, file = "Seurat_EXP.Robj")
-OUT=as.matrix(EXP@data)
+raw_exp_data= as.matrix(EXP@data)
+tmm=edgeR::calcNormFactors(raw_exp_data)
+exptmm=edgeR::cpm(raw_exp_data, lib.size = tmm * colSums(raw_exp_data))
+logexp <- log2(exptmm + 1)
+
+OUT=logexp 
 write.table(file='normalized_exp.txt',OUT,row.names=T,col.names=T,quote=F,sep='\t')
+save(EXP, file = "Seurat_EXP.Robj")
