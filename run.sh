23a70aa79431b01fa53e98ffa583a0e58cd59619
Zhang, Feng <jumphone@163.com>
2018-06-13 13:26:35 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,9 +8,7 @@ PERCENT=0.1
 PYTHON="python"
 RSCRIPT="Rscript"
 
-$PYTHON step2_BuildIndex.py $NET_FILE $NORM_EXP $OUTPUT\.data $PERCENT
-$PYTHON step3_CalZmat.py $OUTPUT\.data $OUTPUT\.data.zmat $CPU
-#$PYTHON step4_rmSymDist.py $OUTPUT\.data.zmat $OUTPUT\.data.zmat.nosym
-#$RSCRIPT step5_deMix.R $NORM_EXP $OUTPUT\.data.zmat.nosym $OUTPUT\.data.zmat.nosym.gca_result $CPU $SEED
-$RSCRIPT step5_deMix.R $NORM_EXP $OUTPUT\.data.zmat $OUTPUT\.data.zmat.gca_result $CPU $SEED
+$PYTHON step1_BuildIndex.py $NET_FILE $NORM_EXP $OUTPUT\.data $PERCENT
+$PYTHON step2_CalZmat.py $OUTPUT\.data $OUTPUT\.data.zmat $CPU
+$RSCRIPT step3_deMix.R $NORM_EXP $OUTPUT\.data.zmat $OUTPUT\.data.zmat.gca_result $CPU $SEED
 
