e688e1797900d82b6009f4c4005a898077fe1e06
Zhang, Feng <jumphone@163.com>
2018-06-21 18:14:35 -0400

    Update stem_score.R

--- tmp/stem_score.R
+++ tmp/stem_score.R
@@ -1,6 +1,6 @@
 exp_data=read.table('GSE70630_OG_processed_data_v2.txt.cleaned.txt', header=T, row.names=1, check.names=FALSE,sep='\t')
-stem_non_EGFR=read.table('STEM.txt',row.names=1)
-ST=which(rownames(exp_data) %in% stem_non_EGFR[,1])
+stem=read.table('STEM.txt')
+ST=which(rownames(exp_data) %in% stem[,1])
 
 s_exp_data=apply(exp_data,1,scale)
 s_s_exp_data=apply(s_exp_data,1,scale,scale=F,center=T)
