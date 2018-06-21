4553d301847e4da104c25974cd605f5a8500c429
Zhang, Feng <jumphone@163.com>
2018-06-21 18:32:44 -0400

    Update go.R

--- tmp/go.R
+++ tmp/go.R
@@ -1,18 +1,23 @@
 library(gplots)
 a=as.matrix(read.table('MODE_MAT.txt',header=T,row.names=1))
-stem_score=read.table('')
+stem_score=read.table('stem_score.txt')[,2]
 
 SUM=apply(a,2,sum)
-B= which(SUM>=3)
+B= which(SUM>=2)
 b=a[,B]
+stem_score_b=stem_score[B]
+
 RSUM=apply(b,1,sum)
 b=b[which(RSUM>0),]
-heatmap(b,Rowv=T,Colv=F,scale='row',labCol='',margins=c(10,10))
+#heatmap(b,Rowv=T,Colv=F,scale='row',labCol='',margins=c(10,10))
 all_gene=rownames(b)
 
 library(Seurat)
 
 EXP = CreateSeuratObject(raw.data = b, min.cells = 0, min.genes=0)
+
+EXP <- AddMetaData(object = EXP, metadata = stem_score_b, col.name = "stem.score")
+EXP@meta.data$stem.score=stem_score_b
 
 EXP=NormalizeData(object = EXP, normalization.method = "LogNormalize", scale.factor = 10000)
 
@@ -25,12 +30,14 @@ PCElbowPlot(object = EXP,num.pc=PCNUM)
 PCUSE=1:3
 EXP = RunTSNE(object = EXP, dims.use = PCUSE, do.fast = TRUE,check_duplicates = FALSE )
 
-RES=0.3
+RES=0.2
 EXP <- FindClusters(object = EXP, reduction.type = "pca", dims.use = PCUSE,  resolution = RES, print.output = 0, save.SNN = TRUE,force.recalc =T)
 
 TSNEPlot(object = EXP,do.label=T)
 
+VlnPlot(object = EXP, features.plot = c('stem.score'))
 
+EXP@scale.data=as.matrix(EXP@data)
 pbmc=EXP
 library(dplyr)
 pbmc.markers <- FindAllMarkers(object = pbmc, only.pos = TRUE, min.pct = 0.1, thresh.use = 0.2)
