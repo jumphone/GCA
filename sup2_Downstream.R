acdd186ae8235d1858cf7307e4e98f5d777e29f0
Zhang, Feng <jumphone@163.com>
2018-07-13 14:05:37 -0400

    Update sup2_Downstream.R

--- sup2_Downstream.R
+++ sup2_Downstream.R
@@ -40,7 +40,7 @@ pbmc.markers %>% group_by(cluster) %>% top_n(2, avg_logFC)
 top10 <- pbmc.markers %>% group_by(cluster) %>% top_n(10, avg_logFC)
 DoHeatmap(object = pbmc, genes.use = top10$gene, slim.col.label = TRUE, remove.key = TRUE,col.low = "grey90", col.mid = "grey75", col.high = "red",cex.row=6 )
 
-pdf('OUTPUT.pdf',width=15,height=15)
+pdf('OUTPUT.pdf',width=7,height=7)
 TSNEPlot(object = EXP,do.label=T)
 DoHeatmap(object = pbmc, genes.use = top10$gene, slim.col.label = TRUE, remove.key = TRUE,col.low = "grey90", col.mid = "grey90", col.high = "red",cex.row=6 )
 dev.off()
