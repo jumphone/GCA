3bc85fac3dc83ec747cc9b411721c75805c26745
Zhang, Feng <jumphone@163.com>
2018-07-18 17:43:43 -0400

    Update 3Seurat_ori.R

--- app/1TF/DEMO/3Seurat_ori.R
+++ app/1TF/DEMO/3Seurat_ori.R
@@ -1,39 +1,3 @@
-library(gplots)
-library(Seurat)
-
-exp_data=as.matrix(read.table('GSE70630_OG_processed_data_v2.txt.cleaned.txt',header=T,row.names=1))
-tf_ident=read.table('IDENT.txt')
-
-new_exp_data=exp_data[,which(colnames(exp_data) %in% tf_ident[,1])]
-
-EXP = CreateSeuratObject(raw.data = new_exp_data, min.cells = 0, min.genes=0)
-
-
-tmp_ident=tf_ident[,2]
-tmp_ident=as.factor(tmp_ident)
-names(tmp_ident)=names(EXP@ident)
-EXP@ident=tmp_ident
-head(EXP@ident)
-
-
-
-pbmc=EXP
-library(dplyr)
-pbmc.markers <- FindAllMarkers(object = pbmc, only.pos = TRUE, min.pct = 0.25, thresh.use = 0.1)
-
-pbmc.markers %>% group_by(cluster) %>% top_n(2, avg_logFC)
-
-write.table(file='EXP_MARKER.txt', pbmc.markers,row.names=T,col.names=T,quote=F,sep='\t')
-
-top10 <- pbmc.markers %>% group_by(cluster) %>% top_n(10, avg_logFC)
-
-all_gene=rownames(EXP@data)
-EXP = ScaleData(object = EXP, genes.use = all_gene)
-
-
-pdf('GENE_MARKER.pdf',width=15,height=15)
-DoHeatmap(object = pbmc, genes.use = top10$gene, slim.col.label = TRUE, remove.key = TRUE,col.low = "grey90", col.mid = "grey75", col.high = "red",cex.row=6 )
-dev.off()
 
 
 ################################################################################
@@ -125,6 +89,47 @@ dev.off()
 
 
 
+###########
+###########
+###########
+###########
+###########
+library(gplots)
+library(Seurat)
+
+exp_data=as.matrix(read.table('GSE70630_OG_processed_data_v2.txt.cleaned.txt',header=T,row.names=1))
+tf_ident=read.table('IDENT.txt')
+
+new_exp_data=exp_data[,which(colnames(exp_data) %in% tf_ident[,1])]
+
+EXP = CreateSeuratObject(raw.data = new_exp_data, min.cells = 0, min.genes=0)
+
+
+tmp_ident=tf_ident[,2]
+tmp_ident=as.factor(tmp_ident)
+names(tmp_ident)=names(EXP@ident)
+EXP@ident=tmp_ident
+head(EXP@ident)
+
+
+
+pbmc=EXP
+library(dplyr)
+pbmc.markers <- FindAllMarkers(object = pbmc, only.pos = TRUE, min.pct = 0.25, thresh.use = 0.1)
+
+pbmc.markers %>% group_by(cluster) %>% top_n(2, avg_logFC)
+
+write.table(file='EXP_MARKER.txt', pbmc.markers,row.names=T,col.names=T,quote=F,sep='\t')
+
+top10 <- pbmc.markers %>% group_by(cluster) %>% top_n(10, avg_logFC)
+
+all_gene=rownames(EXP@data)
+EXP = ScaleData(object = EXP, genes.use = all_gene)
+
+
+pdf('GENE_MARKER.pdf',width=15,height=15)
+DoHeatmap(object = pbmc, genes.use = top10$gene, slim.col.label = TRUE, remove.key = TRUE,col.low = "grey90", col.mid = "grey75", col.high = "red",cex.row=6 )
+dev.off()
 
 
 
